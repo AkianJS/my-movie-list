9d4e4a2f8e636fc6ee259d12abcca28fa4ebc5db
AkianJS <akianzax@gmail.com>
2023-02-05 17:53:42 -0300

    Carousel added

--- src/components/CarouselComp.svelte
+++ src/components/CarouselComp.svelte
@@ -1,1 +1,32 @@
-<h1>Hola</h1>
+<script lang="ts">
+  import { Splide, SplideSlide, SplideTrack } from "@splidejs/svelte-splide";
+  import "@splidejs/svelte-splide/css";
+  export let images: { title: string; src: string }[];
+</script>
+
+<header class="max-w-5xl mx-auto">
+  <Splide
+    options={{ rewind: true }}
+    hasTrack={false}
+    aria-label="Header Slider"
+  >
+    <div class="custom-wrapper max-w-5xl mx-auto">
+      <SplideTrack>
+        {#each images as item}
+          <SplideSlide>
+            <img
+              class="mx-auto"
+              width="700"
+              height="300"
+              src={item.src}
+              alt={item.title}
+            />
+          </SplideSlide>
+        {/each}
+      </SplideTrack>
+    </div>
+  </Splide>
+</header>
+
+<style>
+</style>
