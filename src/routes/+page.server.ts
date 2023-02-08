99c4868cf34113d6d0545b32b8028749b0312730
AkianJS <akianzax@gmail.com>
2023-02-08 03:10:11 -0300

    Home movies and series changed.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,8 +5,8 @@ import type { SeriesInterface } from "../interface/Serie";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = (async () => {
-  const movies: MovieInterface = await getMovies("/discover/movie?language=es");
-  const series: SeriesInterface = await getMovies("/discover/tv?language=es");
+  const movies: MovieInterface = await getMovies("/discover/movie?language=es&sort_by=revenue.desc");
+  const series: SeriesInterface = await getMovies("/discover/tv?language=es&sort_by=vote_average.desc");
   const upcoming: MovieInterface = await getMovies("/movie/upcoming");
   const backdrops = upcoming.results
     .filter((item) => item.backdrop_path !== null)
@@ -15,7 +15,6 @@ export const load: PageServerLoad = (async () => {
       title: item.title,
     }));
 
-    console.log(backdrops)
   if (movies) {
     return {
       movies,
